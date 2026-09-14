8f1bc97c6495a9ed777c0db1fc9040b38dbf22c8
V <valeriya.i.ten@gmail.com>
2026-09-14 16:27:42 -0700

    Added get_unique_watched and get_friends_unique_watched functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -7,12 +7,45 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
-
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+def get_friends_watched_movies(user_data): 
+    friends_watched = []
+    for friend in user_data["friends"]: 
+        for movie in friend["watched"]: 
+            friends_watched.append(movie)
+
+    return friends_watched
+    
+def get_unique_watched(user_data):
+
+    # user_watched and friends_watched are lists of movie dictionaries
+    user_watched = user_data["watched"]     
+    friends_watched = get_friends_watched_movies(user_data)
+
+    unique_list = []                        
+    for movie in user_watched: 
+        if movie not in friends_watched:
+            unique_list.append(movie)
+
+    return unique_list
+
+def get_friends_unique_watched(user_data):
+
+    # user_watched and friends_watched are lists of movie dictionaries
+    user_watched = user_data["watched"]
+    friends_watched = get_friends_watched_movies(user_data)
+
+    unwatched_list = []
+    for movie in friends_watched: 
+        if movie not in user_watched and movie not in unwatched_list: 
+            unwatched_list.append(movie)
+
+    return unwatched_list
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
