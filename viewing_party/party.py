505fbc1b39be029a07fcbe88536a1e34acad5d7b
V <valeriya.i.ten@gmail.com>
2026-09-17 13:29:16 -0700

    Remove debug comments, edit variable names for clarity

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -97,7 +97,6 @@ def get_genre_counts(watched_list):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-
 def get_friends_watched_movies(user_data):
     """helper function. Find the movies that friends have watched
     Returns:
@@ -116,14 +115,12 @@ def get_unique_watched(user_data):
     Returns:
         lists of movie dictionaries [{},{}]
     """
-
-    user_watched = user_data["watched"]  # all the movies the user watched
+    user_watched = user_data["watched"]
     friends_watched = get_friends_watched_movies(
         user_data
-    )  # all the movies that friends watched
+    )
 
     unique = []
-    # filter out the movies that the friends watched
     for movie in user_watched:
         if movie not in friends_watched:
             unique.append(movie)
@@ -136,14 +133,12 @@ def get_friends_unique_watched(user_data):
     Returns:
         lists of movie dictionaries [{},{}]
     """
-
-    user_watched = user_data["watched"]  # all the movies the user watched
+    user_watched = user_data["watched"]
     friends_watched = get_friends_watched_movies(
         user_data
-    )  # all movies that friends watched
+    )
 
     unwatched = []
-    # filter out the movies the user has watched and duplicates
     for movie in friends_watched:
         if movie not in user_watched and movie not in unwatched:
             unwatched.append(movie)
@@ -155,7 +150,6 @@ def get_friends_unique_watched(user_data):
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 
-
 def get_available_recs(user_data):
     """Find recommended movies that the user has not watched but at least one
     of the friends has watched. Only keep movies hosted on subscribed services
@@ -163,42 +157,36 @@ def get_available_recs(user_data):
     Returns:
         lists of movie dictionaries [{},{}]
     """
-
-    unwatched_list = get_friends_unique_watched(
+    friends_watched_unique = get_friends_unique_watched(
         user_data
-    )  # unwatched movies that at least one of the friends watched
+    )
 
-    recommended_list = []
-    # filter out movies on the platforms the user is not subscribed to
-    for movie in unwatched_list:
+    recommended = []
+    for movie in friends_watched_unique:
         if movie["host"] in user_data["subscriptions"]:
-            recommended_list.append(movie)
+            recommended.append(movie)
 
-    return recommended_list
+    return recommended
 
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
-
 def get_new_rec_by_genre(user_data):
     """Find the movies that the user has not watched but at least one of the friends watched
     Only keep movies of the user's most watched genre
 
     Returns:
-        None if the friends' watched list is empty
         lists of movie dictionaries [{},{}]
     """
-    unwatched_list = get_friends_unique_watched(
+    friends_watched_unique = get_friends_unique_watched(
         user_data
-    )  # unwatched movies that at least one of the friends watched
+    ) 
     genre = get_most_watched_genre(user_data)
-    # genre = "Fantasy" #for testing purposes
 
     recommended = []
-    # filter out movies of all genres except the favorite genre
-    for movie in unwatched_list:
+    for movie in friends_watched_unique:
         if movie["genre"] == genre:
             recommended.append(movie)
 
@@ -214,7 +202,6 @@ def get_rec_from_favorites(user_data):
     unique_watched = get_unique_watched(user_data)
 
     recommended = []
-    # filter out movies that friends watched and duplicates
     for movie in favorites:
         if movie in unique_watched and movie not in recommended:
             recommended.append(movie)
